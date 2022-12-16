478bd3c18581d6c7f8b07ee73d3656725d3bcdd5
Kalvin <kalpin1239@gmail.com>
2022-12-16 13:55:46 +0700

    update kalvin file

--- assets/scss/kalvin.scss
+++ assets/scss/kalvin.scss
@@ -1,6 +1,6 @@
 /*!
 =========================================================
-* JohnDoe Landing page
+* Kalvin Landing page
 =========================================================
 
 * Copyright: 2019 DevCRUD (https://devcrud.com)
